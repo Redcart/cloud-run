37b1f4e502daf7e70c4a81230ae339750022ba2b
Simon Corde <40165569+Redcart@users.noreply.github.com>
2026-04-18 22:20:58 +0200

    add configuration for basic cloud run service with hello world fast api app

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/test")
+async def test():
+    return {"message": "Hello World from /test"}
