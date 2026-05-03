3ce65e4231428dee12ca877842e193e10df15303
Simon Corde <40165569+Redcart@users.noreply.github.com>
2026-05-03 19:14:47 +0200

    reorganize app code

--- main.py
+++ main.py
@@ -19,35 +19,27 @@ def get_secret(secret_name: str, version: str = "latest") -> str:
     response = client.access_secret_version(request={"name": secret_path})
     return response.payload.data.decode("UTF-8")
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/test")
-async def test():
-    return {"message": "Hello World from /test"}
-
 
 # Non sensitive environment variables injected at deployment time 
 @app.get("/display/env-variables")
 async def display_env_variables():
     return {"message": f"INPUT_DATASET: {INPUT_DATASET}, OUTPUT_DATASET: {OUTPUT_DATASET}"}
 
+# Sensitive environment variable retrieved at deployment time from GitHub Secrets
+@app.get("/display/secrets-github")
+async def display_secrets_github():
+    return {"message": f"SECRET_VAR from GitHub Secrets: {GITHUB_SECRET_VAR}"}
+
 # Sensitive environment variable injected at deployment time from Secret Manager
 @app.get("/display/secrets")
 async def display_secrets():
     return {"message": f"API_KEY: {API_KEY}"}
-
 
 # Sensitive environment variable retrieved at runtime from Secret Manager
 @app.get("/display/secrets-runtime")
 async def display_secrets_runtime():
     password = get_secret(secret_name="PASSWORD")
     return {"message": f"PASSWORD: {password}"}
-
-# Sensitive environment variable retrieved at runtime from GitHub Secrets
-@app.get("/display/secrets-github")
-async def display_secrets_github():
-    return {"message": f"SECRET_VAR from GitHub Secrets: {GITHUB_SECRET_VAR}"}
